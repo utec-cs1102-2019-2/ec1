e0e0887873fc3ab5585e07759b79baf8dbdd0b4c
jbellidoutec <36900746+jbellidoutec@users.noreply.github.com>
2019-09-12 10:03:21 -0500

    usando punteros

--- session4b/main.cpp
+++ session4b/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+void pedir_notas(float &a, float &b);
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    float pc1 = 0.0;
+    float bonus = 0.0;
+    pedir_notas(pc1, bonus);
+    std::cout << pc1 << "\t" << bonus;
+    //Implementa aqui los punteros
+    float *ptr_pc1 = &pc1;
+    *ptr_pc1 = pc1 + bonus;
+    std::cout << *ptr_pc1;
     return 0;
+}
+
+void pedir_notas(float &a, float &b){
+    std::cin >> a;
+    std::cin >> b;
+    return;
 }
