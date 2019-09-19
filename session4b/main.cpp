e6dcc67d43448ee77ebe09d6ab65c4bd0a1924aa
jbellidoutec <36900746+jbellidoutec@users.noreply.github.com>
2019-09-19 09:23:46 -0500

    session4b

--- session4b/main.cpp
+++ session4b/main.cpp
@@ -7,7 +7,7 @@ int main() {
     float pc1 = 0.0;
     float bonus = 0.0;
     pedir_notas(pc1, bonus);
-    std::cout << pc1 << "\t" << bonus;
+    std::cout << pc1 << "\t" << bonus <<std::endl;
     //Implementa aqui los punteros
     float *ptr_pc1 = &pc1;
     *ptr_pc1 = pc1 + bonus;
